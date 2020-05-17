43337e7a199e199350493957310cf0cfe67400b6
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-05-16 20:05:33 -0700

    logics for navbar

---
+++ components/_App/Header.jsx
@@ -0,0 +1,73 @@
+import { Menu, Container, Image , Icon} from "semantic-ui-react";
+import { useRouter } from "next/router"; //router to get current page value
+import Link from "next/link";
+
+const isActive= route =>  {
+    return route === useRouter().pathname;
+}
+
+const Header = () => {
+    let user=false;
+    return (
+        <Menu fluid inverted>
+            <Container>
+            {/* home */}
+                <Link href="/">
+                    <Menu.Item header active={isActive("/")}>
+                        <Image size="mini" src="/static/farm.png" style={{height: "40px", width: "40px"}} />
+                    Farm
+                </Menu.Item>
+                </Link>
+
+            {/* cart */}
+                <Link href="/cart">
+                    <Menu.Item header active={isActive("/cart")}>
+                        <Icon name="cart" size="large" />
+                    Cart
+                    </Menu.Item>
+                </Link>
+
+                {/* user */}
+                {user && <Link href="/create">
+                    <Menu.Item header active={isActive("/create")}>
+                        <Icon name="add square" size="large" />
+                    Create
+                    </Menu.Item>
+                </Link>}
+
+                {/* account */}
+                { user ? (<>
+                    <Link href="/account">
+                        <Menu.Item header active={isActive("/account")}>
+                            <Icon name="user" size="large" />
+                        Account
+                        </Menu.Item>
+                    </Link>
+                    {/* sign out */}
+                    <Menu.Item header>
+                        <Icon name="sign out" size="large" />
+                        Sign out
+                    </Menu.Item>
+                </>):
+
+                (<>
+                    {/* sign in */}
+                    <Menu.Item header>
+                        <Icon name="sign in" size="large" />
+                        Sign in
+                    </Menu.Item>
+
+                    {/* sign up*/}
+                    <Link href="/signup">
+                        <Menu.Item header active={isActive("/signup")}>
+                            <Icon name="signup" size="large" />
+                        Sign up
+                        </Menu.Item>
+                    </Link>
+                </>) }
+
+            </Container>
+        </Menu>
+    );
+}
+export default Header;
